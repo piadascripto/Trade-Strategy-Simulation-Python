96fad201f32df1d63c15bef0dba80a502adf9731
piadascripto <nrkneip@gmail.com>
2023-07-11 01:38:52 +0000

    Better variable names

--- main.py
+++ main.py
@@ -5,50 +5,50 @@ def calculate_trades():
 	broken = -investment
 	trades_day = 4.0
 	strategy_efficiency = 0.85
-	trade_amount = 5 / 100
-	highest_trade_amount = 10000
+	order_amount = 5 / 100
+	highest_order_amount = 10000
 	gain_percentage = 0.12
 	loss_percentage = -0.30
 	investment_goal = 1000000
 
 	trades = []
-	num_trade = 0
+	trades_quantity = 0
 
 	def array_trades(investment):
-		nonlocal num_trade
+		nonlocal trades_quantity
 		highest_amount = investment
-		num_trade = 0
+		trades_quantity = 0
 
 		def get_trade_amount():
-			_trade_amount = trade_amount * investment
-			actual_trade_amount = min(_trade_amount, highest_trade_amount)
-			return actual_trade_amount
+			_order_amount = order_amount * investment
+			actual_order_amount = min(_order_amount, highest_order_amount)
+			return actual_order_amount
 
 		while investment <= investment_goal and investment > broken:
 			trade_result = gain_percentage * get_trade_amount() if random.random() < strategy_efficiency else loss_percentage * get_trade_amount()
 			investment += trade_result
 			highest_amount = max(highest_amount, investment)
 			trades.append(trade_result)
-			num_trade += 1
+			trades_quantity += 1
 
 		return trades
 
-	all_array_trades = []
+	all_simulated_trades = []
 	for i in range(1000):
 		trades = array_trades(investment)
-		all_array_trades.append({
-			'numTrade': num_trade,
-			'trades': trades
+		all_simulated_trades.append({
+			'Quantity_of_trades': trades_quantity,
+			'Trades': trades
 		})
 
-	lowest_num_trade = min(item['numTrade'] for item in all_array_trades)
-	max_num_trade = max(item['numTrade'] for item in all_array_trades)
+	min_trades_quantity = min(item['Quantity_of_trades'] for item in all_simulated_trades)
+	max_trades_quantity = max(item['Quantity_of_trades'] for item in all_simulated_trades)
 	print("======To achieve USD " + str(investment_goal) + "===========")
-	print("Lowest number of trades:", lowest_num_trade)
-	print("Maximum number of trades:", max_num_trade)
+	print("Lowest number of trades:", min_trades_quantity)
+	print("Maximum number of trades:", max_trades_quantity)
 	print("=========Years===========")
-	print("Min of years to achive:", lowest_num_trade / trades_day / 252)
-	print("Max of years to achive:", max_num_trade / trades_day / 252)
+	print("Min of years to achive:", min_trades_quantity / trades_day / 252)
+	print("Max of years to achive:", max_trades_quantity / trades_day / 252)
 
 
 calculate_trades()
